25dd799cec88ebdffbd9c9376d146e66bb28f753
George Flores <georgeflores886@gmail.com>
2020-06-09 16:39:15 -0400

    updated server js

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
 const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+  
