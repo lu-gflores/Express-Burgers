587c6fb5d72c85e8b07a1f4426e4e41adab26850
George Flores <georgeflores886@gmail.com>
2020-06-09 15:29:19 -0400

    added db and sql files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES('Royale with Cheese');
+
+INSERT INTO burgers (burger_name)
+VALUES('Turkey Burger');
+
+INSERT INTO burgers (burger_name)
+VALUES('Bison Burger');
