f2f34c6146143e04c3c100d02f55d2bcbec46e3f
George Flores <georgeflores886@gmail.com>
2020-06-09 15:58:05 -0400

    added orm, models to repo

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+const connection = require('./connection.js');
+
+const orm = {
+    selectAll: function() {
+
+    },
+    insertOne: function() {
+
+    },
+    updateOne: function() {
+        
+    }
+}
+
+
+module.exports = orm;
